ede42fcbc3f00e70e8ddeab7f9866b5671e685ff
nizarramzy <nizarramzy07@gmail.com>
2026-02-19 10:15:03 +0700

    a

---
+++ jobsheet2/mahasiswa21.java
@@ -0,0 +1,45 @@
+package jobsheet2;
+
+public class mahasiswa21 {
+    public static void main(String[] args) {
+        
+    }
+        String nim;
+        String nama;
+        String kelas;
+        double ipk;
+
+        void tampilkanInformasi(){
+            System.out.println("Nama " + nama);
+            System.out.println("NIM   : " + nim);
+            System.out.println("IPK   : " + ipk);
+            System.out.println("Kelas : " + kelas);
+
+        }
+    
+    
+        void ubahKelas(String kelasBaru) {
+            kelas = kelasBaru;
+
+        }
+    
+    void updateIpk(double ipkBaru) {
+        if (ipkBaru >= 0.0 && ipkBaru <= 4.0) {
+            ipk = ipkBaru;
+        } else {
+            System.out.println("IPK tidak valid. Harus antara 0.0 dan 4.0");
+        }
+    }
+
+        String nilaiKinerja() {
+        if (ipk >= 3.5) {
+            return "Kinerja sangat baik";
+        } else if (ipk >= 3.0) {
+            return "Kinerja baik";
+        } else if (ipk >= 2.0) {
+            return "Kinerja cukup";
+        } else {
+            return "Kinerja kurang";
+        }
+    }
+    }
